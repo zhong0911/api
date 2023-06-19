e02e10f072ca768f7011a41c8e850312d6a36b5c
zhong0911 <i@antx.cc>
2023-06-19 19:38:49 +0800

    added new functions

--- api/myaddr/index.php
+++ api/myaddr/index.php
@@ -1,2 +1,25 @@
 <?php
-echo $_SERVER['REMOTE_ADDR'];
+/**
+ * @Author: zhong
+ * @Date: 2023-06-19 17-42-24
+ * @LastEditors: zhong
+ */
+
+error_reporting(0);
+date_default_timezone_set("Asia/Shanghai");
+header("Access-Control-Allow-Origin:*");
+header("Content-type:application/json; charset=utf-8");
+
+
+require_once '../../vendor/autoload.php';
+require_once '../../core/ip/query.php';
+require_once '../../db/db.php';
+
+
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
+if ($REQUEST_METHOD === "GET" || $REQUEST_METHOD === "POST") {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    echo queryIpv4($ip);
+} else {
+    echo json_encode(array('success' => false, "message" => 'No POST or GET method'), true);
+}
